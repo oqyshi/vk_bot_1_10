9d5661a52d6bdba18b60a244b66d955215ee153e
Anvar <33487144+oqyshi@users.noreply.github.com>
2020-04-25 16:03:43 +0600

    Update vk_bot_1_10.py

--- vk_bot_1_10.py
+++ vk_bot_1_10.py
@@ -4,39 +4,39 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
+vk_session = vk_api.VkApi(
+    token=TOKEN)
+
+longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+vk = vk_session.get_api()
+
+day = {0: 'понедельник', 1: 'вторник', 2: 'среда', 3: 'четверг',
+       4: 'пятница', 5: 'суббота', 6: 'воскресенье'}
+
+
+def weekday_response(request_data):
+    date = datetime.datetime.strptime(request_data, '%Y-%m-%d').weekday()
+    return day[date]
+
+
+def help():
+    return f"Write the date in the format 'YYYY-MM-DD' and I will say the day of the week."
+
 
 def main():
-    vk_session = vk_api.VkApi(token=TOKEN)
-
-    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
-    vk = vk_session.get_api()
-
-    dt = datetime.datetime.now()
-
-    day = {0: 'понедельник', 1: 'вторник', 2: 'среда', 3: 'четверг',
-           4: 'пятница', 5: 'суббота', 6: 'воскресенье'}
-
+    flag_data, flag_help = False, True
     for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            if event.obj.message:
-                date, time, weekday = dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S'), dt.weekday()
-                if "время" in event.obj.message['text'].lower() or "число" in event.obj.message['text'].lower() or \
-                        "дата" in event.obj.message['text'].lower() or "день" in event.obj.message['text'].lower():
-                    vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message=f"Сегодня {date}\n{day[weekday]}\nСейчас {time}",
-                                     random_id=random.randint(0, 2 ** 64))
-                else:
-                    vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message='Я могу сообщить дату, время и день недели, если Вы напишете одно из этих слов',
-                                     random_id=random.randint(0, 2 ** 64))
-        elif event.type == VkBotEventType.MESSAGE_REPLY:
-            if event.obj.message:
-                date, time, weekday = dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S'), dt.weekday()
-                if "время" in event.obj.message['text'].lower() or "число" in event.obj.message['text'].lower() or \
-                        "дата" in event.obj.message['text'].lower() or "день" in event.obj.message['text'].lower():
-                    vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message=f"Сегодня {date}\n{day[weekday]}\nСейчас {time}",
-                                     random_id=random.randint(0, 2 ** 64))
+        if event.type == VkBotEventType.MESSAGE_NEW and flag_help:
+            flag_data = not flag_data
+            flag_help = not flag_help
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=help(),
+                             random_id=random.randint(0, 2 ** 64))
+
+        elif event.type == VkBotEventType.MESSAGE_NEW and flag_data:
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=f"{weekday_response(event.obj.message['text'])}\n\n{help()}",
+                             random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == '__main__':
